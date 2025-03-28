f55bd16bb0820f426a54ea4df0d7c3606abc56b7
Rohith798937 <rohithnagula123@gmail.com>
2025-03-28 19:31:42 +0530

    Update main.py

--- rohith/main.py
+++ rohith/main.py
@@ -199,10 +199,11 @@ def TradeLens():
                 # Display corresponding image
                 image_file = get_rsi_image(current_rsi)
 
-                if os.path.exists(image_file): st.image(image_file)
+                if os.path.exists(image_file): 
+                    st.image(image_file)
             
-                else:
-                st.warning(f"RSI image '{image_file}' not found.")
+                else: 
+                    st.warning(f"RSI image '{image_file}' not found.")
             
         '''except Exception as e:
             st.error("Failed to calculate RSI.")'''
